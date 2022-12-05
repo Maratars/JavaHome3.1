511f98b1d3e4ece8db995399b334e5cb7ba21691
Marat <marars2205@gmail.com>
2022-12-05 17:53:32 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amound = 10000; //рублей
+        int bonusformile = 20; //рублей
+        int bonus = amound / bonusformile; //миль
+        System.out.println(bonus + " миль");
+    }
+}
