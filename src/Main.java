8ad27adb384a4913fdcdad2332f157b42c6d5b79
Marat <marars2205@gmail.com>
2022-12-05 23:14:25 +0400

    src/Main changed

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         int amound = 10000; //рублей
-        int bonusformile = 20; //рублей
-        int bonus = amound / bonusformile; //миль
+        int bonusforMile = 20; //рублей
+        int bonus = amound / bonusforMile; //миль
         System.out.println(bonus + " миль");
     }
 }
